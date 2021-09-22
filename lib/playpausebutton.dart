b86ba28bfa402b6211f71ad60d71d6145a1c6fe4
Michel <micheltv10@gmail.com>
2021-09-22 15:52:32 +0200

    warnings removed

--- lib/playpausebutton.dart
+++ lib/playpausebutton.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:music_player/main.dart';
-import 'package:ocarina/ocarina.dart';
 
 class PlayPauseButtonWidget extends StatefulWidget {
   final bool playing;
   final Player player;
   final int size;
   final BoolConsumer setPlaying;
-  PlayPauseButtonWidget({ Key? key, required this.playing, required this.player, required this.size, required this.setPlaying}) : super(key: key);
+  const PlayPauseButtonWidget({ Key? key, required this.playing, required this.player, required this.size, required this.setPlaying}) : super(key: key);
 
   @override
   _PlayPauseButtonWidgetState createState() => _PlayPauseButtonWidgetState(playing);
@@ -18,10 +17,8 @@ class _PlayPauseButtonWidgetState extends State<PlayPauseButtonWidget> {
   _PlayPauseButtonWidgetState(this.playing);
 @override
   void didUpdateWidget(covariant PlayPauseButtonWidget oldWidget) {
-    // TODO: implement didUpdateWidget
     super.didUpdateWidget(oldWidget);
     playing = widget.playing;
-    print('_playPauseButtonWidgetState.didUpdateWidget playing = $playing');
   }
 
   
@@ -30,7 +27,7 @@ class _PlayPauseButtonWidgetState extends State<PlayPauseButtonWidget> {
   Widget build(BuildContext context) {
     if (playing) {
       return IconButton(
-        icon: Icon(Icons.pause),
+        icon: const Icon(Icons.pause),
         iconSize: widget.size.toDouble(),
         onPressed: () async {
           await widget.player.pause();
@@ -42,7 +39,7 @@ class _PlayPauseButtonWidgetState extends State<PlayPauseButtonWidget> {
       );
     } else {
       return IconButton(
-        icon: Icon(Icons.play_arrow),
+        icon: const Icon(Icons.play_arrow),
         iconSize: widget.size.toDouble(),
         onPressed: () async {
           await widget.player.play();

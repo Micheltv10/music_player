d5583e33d44dd720476765f99fe6ae3f287235c0
Steffen Nowacki <steffen.nowacki@partmaster.de>
2021-09-23 11:35:11 +0200

    add youtube player

--- lib/player.dart
+++ lib/player.dart
@@ -1,41 +1,89 @@
 import 'package:ocarina/ocarina.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
+enum YoutubePlayerState { off, pending, on, }
 class Player {
+
   OcarinaPlayer? currentPlayer;
   OcarinaPlayer? pendingPlayer;
-  Future<void> load(filePath) {
+  YoutubePlayerState youtubePlayerState = YoutubePlayerState.off;
+  final YoutubePlayerController youtubeController;
+  final YoutubePlayer youtubeWidget;
+
+  Player() : this._(YoutubePlayerController(initialVideoId: ''));
+  Player._(YoutubePlayerController controller) : youtubeController = controller, youtubeWidget = YoutubePlayer(controller: controller);
+
+  Future<void> loadUri(Uri uri) {
+    youtubePlayerState = YoutubePlayerState.off;
+      if(pendingPlayer != null) {
+        pendingPlayer?.dispose();
+        pendingPlayer = null;
+      }
+    if(uri.isScheme("file")) {
+    pendingPlayer = OcarinaPlayer(filePath: uri.toFilePath());
+    return pendingPlayer?.load() ?? Future.value(null);
+    }
+    
+    final videoId = YoutubePlayer.convertUrlToId(uri.toString());
+    if(videoId!=null) {
+      youtubeController.load(videoId);
+      youtubePlayerState = YoutubePlayerState.pending;
+    }
+    return Future.value(null);
+  }
+
+  Future<void> load(String filePath) {
+    youtubePlayerState = YoutubePlayerState.off;
     pendingPlayer = OcarinaPlayer(filePath: filePath);
     return pendingPlayer?.load() ?? Future.value(null);
   }
-  Future<void> play(){
+  Future<void> play() {
     if (pendingPlayer != null) {
       if (currentPlayer != null) {
-        currentPlayer?.dispose();
-        
+        currentPlayer?.dispose();      
       }
       currentPlayer = pendingPlayer;
       pendingPlayer = null;
-
+    }
+    if(youtubePlayerState != YoutubePlayerState.off) {
+      if(currentPlayer != null) {
+        currentPlayer?.dispose();
+        currentPlayer = null;
+      }
+      youtubePlayerState = YoutubePlayerState.on;
+      youtubeController.play();
+      return Future.value(null);
     }
     return currentPlayer?.play()  ?? Future.value(null);
   }
   Future<void> pause(){
+    if(youtubePlayerState == YoutubePlayerState.on) {
+      youtubeController.pause();
+      return Future.value(null);
+    }
     return currentPlayer?.pause() ?? Future.value(null);
   }
   void dispose() {
+    youtubeController.dispose();
     currentPlayer?.dispose();
     currentPlayer = null;
     pendingPlayer?.dispose();
     pendingPlayer = null;
   }
   Future<int> position() {
+    if(youtubePlayerState == YoutubePlayerState.on) {
+      return Future.value(youtubeController.value.position.inMilliseconds);
+    }
     return currentPlayer?.position() ?? Future.value(0);
   }
   bool isLoaded() {
-    currentPlayer?.load();
     return currentPlayer?.isLoaded() ?? false;
   }
   Future<void> seek(Duration duration) {
+    if(youtubePlayerState == YoutubePlayerState.on) {
+      youtubeController.seekTo(duration);
+      return Future.value(null);
+    }
     return currentPlayer?.seek(duration) ?? Future.value(null);
   }
 }
---
+++ lib/types.dart
@@ -0,0 +1,121 @@
+import 'dart:ui';
+
+enum ImageKind {
+  cover,
+  notes,
+  artist,
+}
+
+enum AudioKind {
+  pronunciation,
+  accompaniment,
+  song,
+}
+
+enum TextKind {
+  lyrics,
+  phonetics,
+  translation,
+}
+
+class AudioData {
+  final AudioKind kind;
+  final String name;
+  final Uri uri;
+  final Locale locale;
+  final Duration duration;
+
+  AudioData({required this.duration, required this.kind, required this.locale, required this.name, required this.uri});
+}
+
+class TextData {
+  final TextKind kind;
+  final String name;
+  final Uri uri;
+  final Locale locale;
+
+  TextData({required this.locale, required this.kind, required this.name, required this.uri});
+}
+
+class ImageData {
+  final ImageKind kind;
+  final String name;
+  final Uri uri;
+
+  ImageData({required this.kind, required this.name, required this.uri});
+}
+class SongData {
+  final int index;
+  final String title;
+  final String subtitle;
+  final List<AudioData> audios;
+  final List<ImageData> images;
+  final List<TextData> texts;
+
+  SongData({required this.index, required this.subtitle, required this.audios, required this.images, required this.texts, required this.title});
+}
+
+typedef  int SongComparator(SongData left, SongData right);
+class SongComparators {
+    static int compareAscendingByTitle(SongData left, SongData right) => left.title.compareTo(right.title);
+    static int compareDescendingByTitle(SongData left, SongData right) => right.title.compareTo(left.title);
+    static int compareAscendingByIndex(SongData left, SongData right) => left.index.compareTo(right.index);
+    static int compareDescendingByIndex(SongData left, SongData right) => right.index.compareTo(left.index);
+    static int compareNothing(SongData left, SongData right) => 0;
+}
+
+abstract class SongPredicate {
+  bool test(SongData value);
+}
+
+class TitlePredicate implements SongPredicate {
+  final RegExp regExp;
+  TitlePredicate(this.regExp);
+  @override
+  bool test(SongData input) {
+    return regExp.hasMatch(input.title);
+  }
+
+}
+class SongLocalePredicate implements SongPredicate {
+  final Locale locale;
+  SongLocalePredicate(this.locale);
+  @override
+  bool test(SongData input) {
+    return input.audios.where((audio) => audio.kind == AudioKind.song && audio.locale == locale).isNotEmpty;
+  }
+}
+class AndPredicate implements SongPredicate {
+  final Iterable<SongPredicate> predicates;
+  AndPredicate(this.predicates);
+  @override
+  bool test(SongData value) {
+    return predicates.fold(true, (previousValue, predicate) => previousValue && predicate.test(value));
+  }
+}
+class OrPredicate implements SongPredicate {
+  final Iterable<SongPredicate> predicates;
+  OrPredicate(this.predicates);
+  @override
+  bool test(SongData value) {
+    return predicates.fold(false, (previousValue, predicate) => previousValue || predicate.test(value));
+  }
+}
+class TruePredicate implements SongPredicate {
+  const TruePredicate();
+  @override
+  bool test(SongData value) {
+    return true;
+  }
+  static const instance = TruePredicate(); 
+}
+class SongProvider {
+  final Iterable<SongData> songs;
+  SongProvider(this.songs);
+  Iterable<SongData> filterAndSort({
+    SongPredicate predicate=TruePredicate.instance, 
+    SongComparator comparator=SongComparators.compareNothing,
+    }) {
+      return songs.where((song) => predicate.test(song)).toList()..sort(comparator);
+  }
+}
